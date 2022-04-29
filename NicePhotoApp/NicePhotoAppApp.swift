1b8d25bf6e96def79c54f4c57553dcf3930f89ff
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-04-29 00:51:12 -0300

    Initial commit

---
+++ NicePhotoApp/NicePhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NicePhotoAppApp.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NicePhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
