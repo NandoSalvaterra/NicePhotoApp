c324bf605a4fb7ea301699655bff9010d0e4f596
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-05-09 11:08:28 -0300

    Create PhotosRepository.swift

---
+++ NicePhotoApp/Data/Photos/PhotosRepository.swift
@@ -0,0 +1,73 @@
+//
+//  PhotosRepository.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 07/05/22.
+//
+
+import UIKit
+import Combine
+
+class PhotosRepository: PhotosRepositoryProtocol {
+    
+    let remoteData: PhotosRemoteDataProtocol
+    let localData: PhotosLocalDataProtocol
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        self.remoteData = PhotosRemoteData()
+        self.localData = PhotosLocalData()
+    }
+    
+    func getGalleryPhotos() -> AnyPublisher<[Photo], RepositoryError> {
+        return localData.getGalleryPhotos()
+            .map { $0.map { .init(phAsset: $0) } }
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func getGooglePhotos(token: String) -> AnyPublisher<[Photo], RepositoryError> {
+        return remoteData.getGooglePhotos(token: token)
+            .map { $0.map { .init(googlePhoto: $0) } }
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func getFavoritePhotos() -> AnyPublisher<[Photo], RepositoryError> {
+        return localData.getFavoritePhotos()
+            .map { $0.map { .init(phAsset: $0) } }
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func favoritePhoto(_ photo: Photo) -> AnyPublisher<Bool, RepositoryError> {
+        return localData.favoritePhoto(photo)
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func dismissPhoto() -> AnyPublisher<String, RepositoryError> {
+        return Future { promise in
+            
+        }.eraseToAnyPublisher()
+    }
+    
+    func downloadPhoto(url: URL) -> AnyPublisher<UIImage, RepositoryError> {
+        return remoteData.downloadPhoto(url: url)
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func checkDuplicatedPhoto(image: UIImage, fileName: String) -> AnyPublisher<(Bool, UIImage), RepositoryError> {
+        return localData.checkDuplicatedPhoto(image: image, fileName: fileName)
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func savePhotoInGallery(image: UIImage) -> AnyPublisher<Bool, RepositoryError> {
+        return localData.savePhotoInGallery(image: image)
+            .mapError { .customError($0) }
+            .eraseToAnyPublisher()
+    }
+}
