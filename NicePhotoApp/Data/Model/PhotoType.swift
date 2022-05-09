3a3bd59fc99267efb0eaf7c816c9281defa48431
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-05-09 11:05:52 -0300

    Create PhotoType.swift

---
+++ NicePhotoApp/Data/Model/PhotoType.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoType.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 08/05/22.
+//
+
+import Foundation
+
+enum PhotoType {
+
+    case google
+    case gallery
+}
