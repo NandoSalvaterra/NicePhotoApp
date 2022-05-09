3264b5fd9600b4feeabc785d1206352bbcb60c20
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-05-09 11:12:37 -0300

    Adds RemoteDataError and user placeholder image

---
+++ NicePhotoApp/Remote/Error/RemoteDataError.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteDataError.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 05/05/22.
+//
+
+import Foundation
+
+enum RemoteDataError: Error {
+
+    case authError(Error)
+    case invalidData
+}
