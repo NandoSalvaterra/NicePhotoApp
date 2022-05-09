81dd7a447bb38f91993287d1fd714342ea693b77
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-05-09 11:08:26 -0300

    Create PhotosLocalDataProtocol.swift

---
+++ NicePhotoApp/Data/Photos/Local/PhotosLocalDataProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosLocalDataProtocol.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 07/05/22.
+//
+
+import UIKit
+import Combine
+import Photos
+
+protocol PhotosLocalDataProtocol {
+
+    func getGalleryPhotos() -> AnyPublisher<[PHAsset], LocalDataError>
+    func getFavoritePhotos() -> AnyPublisher<[PHAsset], LocalDataError>
+    func favoritePhoto(_ photo: Photo) -> AnyPublisher<Bool, LocalDataError>
+    func checkDuplicatedPhoto(image: UIImage, fileName: String) -> AnyPublisher<(Bool, UIImage), LocalDataError>
+    func savePhotoInGallery(image: UIImage) -> AnyPublisher<Bool, LocalDataError>
+}
