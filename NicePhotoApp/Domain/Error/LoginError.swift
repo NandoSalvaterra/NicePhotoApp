41e0dcf9070716fc831a4c33e2f2c455e024daf4
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-05-09 11:08:46 -0300

    Adds some errors

---
+++ NicePhotoApp/Domain/Error/LoginError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginError.swift
+//  NicePhotoApp
+//
+//  Created by Luiz Fernando Salvaterra on 05/05/22.
+//
+
+import Foundation
+
+enum LoginError: Error {
+
+    case internalError(Error)
+}
--- NicePhotoApp/Domain/Error/PhotosError.swift
+++ NicePhotoApp/Domain/Error/PhotosError.swift
@@ -1,8 +1,14 @@
 //
-//  GetPhotosError.swift
+//  PhotosError.swift
 //  NicePhotoApp
 //
 //  Created by Luiz Fernando Salvaterra on 03/05/22.
 //
 
 import Foundation
+
+enum PhotosError: Error {
+
+    case emptyToken
+    case custom(Error)
+}
